dcb36288d2bccbbf7418805c118fd0e1396c4b31
Mark Bello <mcbello51286@gmail.com>
2017-12-04 18:44:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,5 +41,9 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data[season].each{|contestant, value|
+    ages = []
+    ages.push(value["age"])
+    }
+  }
 end
