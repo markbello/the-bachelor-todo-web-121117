9dc2a9b949dfbab3bee72ee55093b84f93ea7592
Mark Bello <mcbello51286@gmail.com>
2017-12-04 18:52:27 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,50 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each{|key, value|
+    if key["status"] == "Winner"
+      return key["name"].split[0]
+    end
+  }
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each{|season, value|
+    value.each{|contestant, value|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    }
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each{|season, value|
+    value.each{|contestant, value|
+      if contestant["hometown"] == hometown
+        count +=1
+      end
+    }
+  }
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each{|season, value|
+    value.each{|contestant, value|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = 0
+  data[season].each{|contestant, value|
+    ages += contestant["age"].to_f
+  }
+  ages /= data[season].length
+  ages.round
 end
