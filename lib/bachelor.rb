5ea99b3c61313584b4d523d6c07b060490247e46
Mark Bello <mcbello51286@gmail.com>
2017-12-04 18:23:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,10 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each{|key1, value1|
+    value.each{|key2, value2|
+      if value2["status"] = "Winner"
+        return value2["name"].split[0]
+    }
+  }
 end
 
 def get_contestant_name(data, occupation)
