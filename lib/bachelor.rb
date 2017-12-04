e8c3a87ba7fdccdf7cad61582f86b09e5a94302d
Mark Bello <mcbello51286@gmail.com>
2017-12-04 18:51:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,5 +45,6 @@ def get_average_age_for_season(data, season)
   data[season].each{|contestant, value|
     ages += contestant["age"].to_f
   }
-  ages /= (data[season].length).ceil
+  ages /= data[season].length
+  ages.ceil
 end
